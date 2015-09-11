d5897a8204cc0f7607ed724f39ee39654723cd51
Eduardo Eidelwein Berlitz <eberlitz@gmail.com>
2015-09-11 14:04:20 -0300

    Documentação inicial

---
+++ js/docs-setup.js
@@ -0,0 +1,108 @@
+NG_DOCS={
+  "sections": {
+    "api": "API Documentation"
+  },
+  "pages": [
+    {
+      "section": "api",
+      "id": "core.directive:gvmMessages",
+      "shortName": "gvmMessages",
+      "type": "directive",
+      "moduleName": "core",
+      "shortDescription": "Região para exibir mensagens informativas ou de erros",
+      "keywords": "api core de directive erros exibir informativas mensagens ou para regi"
+    },
+    {
+      "section": "api",
+      "id": "core.service:ajudaService",
+      "shortName": "ajudaService",
+      "type": "service",
+      "moduleName": "core",
+      "shortDescription": "Serviço utilizado para definir seções de ajuda do aplicativo",
+      "keywords": "addsecao ajuda api aplicativo array associado core da de define definir determinado elemento feature gvm-messages inst mensagens method nome objeto objetos obt ou para quest questoes se secao servi service text texto title titulo um uma utilizado"
+    },
+    {
+      "section": "api",
+      "id": "core.service:AppModulos",
+      "shortName": "AppModulos",
+      "type": "service",
+      "moduleName": "core",
+      "shortDescription": "Serviço utilizado para acessar dados referentes a módulos",
+      "keywords": "acessar api contexto core dados de getcontextdata logado method obt para perfil referentes servi service usu utilizado"
+    },
+    {
+      "section": "api",
+      "id": "core.service:dialogo",
+      "shortName": "dialogo",
+      "type": "service",
+      "moduleName": "core",
+      "shortDescription": "Serviço para exibir mensagens em formato popup.",
+      "keywords": "alert ap api bot buttonlabels callback caso confirm confirma conte core da de determinado dos duration em erro error exibe exibida exibir formato fun informativa mensagem mensagens message method nome ocultada para popup posi position ser servi service success sucesso tela tempo title toast uma usu"
+    },
+    {
+      "section": "api",
+      "id": "core.service:gvmMessagesService",
+      "shortName": "gvmMessagesService",
+      "type": "service",
+      "moduleName": "core",
+      "shortDescription": "Serviço utilizado para exibir mensagens informativas ou de erros em elementos gvm-messages",
+      "keywords": "api associado clear core corrente da de define determinado elemento elementos em erro error erros exibida exibir gvm-messages info informativa informativas inst mensagem mensagens message method nome obt obtida ou para remove ser servi service um uma utilizado"
+    },
+    {
+      "section": "api",
+      "id": "core.service:MenuProvider",
+      "shortName": "MenuProvider",
+      "type": "service",
+      "moduleName": "core",
+      "shortDescription": "TODO addMenuItem",
+      "keywords": "addmenuitem api core service todo"
+    },
+    {
+      "section": "api",
+      "id": "core.service:navigationService",
+      "shortName": "navigationService",
+      "type": "service",
+      "moduleName": "core",
+      "shortDescription": "Serviço para controle de navegação",
+      "keywords": "anterior ao api armazenado caso controle core da dado dados data de determinada deve enviados esta exista foi getdata manter method navega navegar obt ou para persist remover se serem servi service uma url volta"
+    }
+  ],
+  "apis": {
+    "api": true
+  },
+  "__file": "_FAKE_DEST_/js/docs-setup.js",
+  "__options": {
+    "startPage": "/api",
+    "scripts": [
+      "js/angular.min.js",
+      "js/angular-animate.min.js",
+      "js/marked.js",
+      "js/libs/angular/angular.min.js",
+      "js/libs/angular-animate/angular-animate.min.js"
+    ],
+    "styles": [],
+    "title": "",
+    "html5Mode": false,
+    "editExample": true,
+    "navTemplate": false,
+    "navContent": "",
+    "navTemplateData": {},
+    "image": "https://gvmobile.azurewebsites.net/preview/assets/logo.png",
+    "imageLink": "https://gvmobile.azurewebsites.net/preview/",
+    "loadDefaults": {
+      "angular": true,
+      "angularAnimate": true,
+      "marked": true
+    }
+  },
+  "html5Mode": false,
+  "editExample": true,
+  "startPage": "/api",
+  "scripts": [
+    "js/angular.min.js",
+    "js/angular-animate.min.js",
+    "js/marked.js",
+    "js/libs/angular/angular.min.js",
+    "js/libs/angular-animate/angular-animate.min.js"
+  ]
+};
